b73b5ff1d85bf853fd667526aaf2e2417bc01af7
Oleh Karmazin <kalemzwork@gmail.com>
2025-08-31 17:19:23 +0200

    feat: user's avatars and user-data with mock users

---
+++ src/app/user/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UsersData } from '../user/user-data';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = UsersData[0];
+}
