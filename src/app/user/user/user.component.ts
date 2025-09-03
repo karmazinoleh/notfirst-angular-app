368f6d927df5a7a0a7873d16d8ab1c94b9f68d7c
Oleh Karmazin <kalemzwork@gmail.com>
2025-09-03 15:37:46 +0200

    feat: refactor user component to use a single user input object

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -8,17 +8,18 @@ import { UsersData } from '../user/user-data';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: number;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: {
+    id: number, 
+    name: string, 
+    avatar: string
+  };
   @Output() select = new EventEmitter<number>();
-  // select = outpit<number>();
 
   get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser(){  
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 }
